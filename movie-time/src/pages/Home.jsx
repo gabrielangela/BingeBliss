d32a1646b6bf15be4b7f71fbcc3da26444e95265
gabrielangela <gabrielangelam29@gmail.com>
2025-06-21 10:09:39 +0700

    Initial Home page content

---
+++ movie-time/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h2>Welcome to BingeBliss</h2>
+      <p>Discover and search your favorite movies!</p>
+    </section>
+  );
+};
+
+export default Home;
